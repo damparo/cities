ec8e694f8a7899249c35e4b8b0ed2f89415ca966
Dennis Amparo <dennisamparo11@gmail.com>
2021-04-16 18:46:31 -0700

    rendering new ciy after new search

--- reactcities/src/components/SearchBar/SearchBar.js
+++ reactcities/src/components/SearchBar/SearchBar.js
@@ -11,20 +11,8 @@ function SearchBar() {
   };
 
   const [showCity, setShowCity] = useState(false);
-  const returnCity = ()=> setShowCity(true);
-
-
-  // return (
-
-  //   <CityDisplay name={cityName} id="cityblock"/>
-  // )
-
-
-
-  
-
-
-
+  const returnCity = () => setShowCity(true);
+  const hidecity = () => setShowCity(false);
 
   return (
     <div>
@@ -39,17 +27,15 @@ function SearchBar() {
             placeholder="search for a city"
             id="search"
             onChange={(e) => setCityName(e.target.value)}
+            onClick={hidecity}
           />
-          <i onClick={returnCity} for="search" className="small material-icons">search</i>
+          <i onClick={returnCity} for="search" className="small material-icons">
+            search
+          </i>
         </div>
       </form>
 
-      { showCity ? 
-      
-      <CityDisplay name={cityName} id="cityblock"
-      />
-      : null
-    }
+      {showCity ? <CityDisplay name={cityName} id="cityblock" /> : null}
     </div>
   );
 }
