52910006eddb806ff58932a5679ba5dce0ee6798
Dennis Amparo <dennisamparo11@gmail.com>
2021-04-21 11:02:16 -0700

    call is grabbing name from searchbar

--- reactcities/src/components/SearchBar/SearchBar.js
+++ reactcities/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import CityDisplay from "../CityDisplay/CityDisplay";
 import Welcome from "../Welcome/Welcome";
+import axios from "axios";
 
 import "./SearchBar.css";
 
@@ -11,11 +12,33 @@ function SearchBar() {
     e.preventDefault();
   };
 
-
-
   const [showCity, setShowCity] = useState(false);
   const returnCity = () => setShowCity(true);
   const hidecity = () => setShowCity(false);
+
+  // Set up grabbing name from  search bar to make call
+
+  const [cityPic, setCityPic] = useState(cityName);
+
+  const handleClick = () =>
+  {
+
+    // console.log("hello");
+    setCityPic(cityName)
+
+  };
+
+  useEffect ( () => {
+
+      axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${cityPic}&key=${process.env.REACT_APP_PLACES_API_KEY}&inputtype=textquery&fields=name,photos`).then( res => {
+        console.log(res)
+      })
+      .catch( err => {
+        console.log(err)
+      })
+
+
+  }, [cityPic])
 
   return (
     <div>
@@ -34,7 +57,7 @@ function SearchBar() {
             onChange={(e) => setCityName(e.target.value)}
             onClick={hidecity}
           />
-          <i onClick={returnCity} for="search" className="small material-icons">
+          <i onClick={handleClick} for="search" className="small material-icons">
             search
           </i>
         </div>
@@ -43,11 +66,7 @@ function SearchBar() {
 
       {showCity ? <CityDisplay name={cityName} id="cityblock" 
       
-      
-      
-      // <i onClick={returnCity} for="search" className="small material-icons">
-      //       search
-      //     </i>
+    
       
       /> : null}
     </div>
